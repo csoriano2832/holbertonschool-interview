e4bfa2b8b2b25b4e136f30df7045a4156cd18e69
Christopher Soriano <2832@holbertonschool.com>
2022-01-20 14:43:14 -0400

    Fix betty errors

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include "binary_trees.h"
 
 /**
@@ -16,9 +15,11 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	new = malloc(sizeof(binary_tree_t));
 	if (new == NULL)
 		return (NULL);
+	
 	new->n = value;
 	new->left = NULL;
 	new->right = NULL;
 	new->parent = parent;
+	
 	return (new);
 }
--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 #include "binary_trees.h"
 
 /**
