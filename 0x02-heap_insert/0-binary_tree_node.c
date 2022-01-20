90b0075909c37cc7c7833b867807dd30c83195c5
Christopher Soriano <2832@holbertonschool.com>
2022-01-20 14:51:39 -0400

    Remove trailing whitespace

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -15,11 +15,9 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	new = malloc(sizeof(binary_tree_t));
 	if (new == NULL)
 		return (NULL);
-	
 	new->n = value;
 	new->left = NULL;
 	new->right = NULL;
 	new->parent = parent;
-	
 	return (new);
 }
