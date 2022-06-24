af7d791607acad34ccbcf3fc152bb5e74b5ffdc0
Christopher Soriano <2832@holbertonschool.com>
2022-06-23 22:34:41 -0400

    Fix formatting

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -19,24 +19,15 @@ def makeChange(coins, total):
         - You can assume you have an infinite number of each denomination of
         coin in the list
     """
-    if total <= 0:
+    if total < 1:
         return 0
-
-    newVal = total + 1
-    store = {0: 0}
-
-    for i in range(1, total + 1):
-        store[i] = newVal
-
-        for coin in coins:
-            current = i - coin
-            if current < 0:
-                continue
-
-            store[i] = min(store[current] + 1, store[i])
-
-    if store[total] == total + 1:
+    change = 0
+    coins.sort(reverse=True)
+    for coin in coins:
+        temp_change = int(total / coin)
+        total -= (temp_change * coin)
+        change += temp_change
+        if total == 0:
+            return change
+    if total != 0:
         return -1
-
-    return store[total]
-    
